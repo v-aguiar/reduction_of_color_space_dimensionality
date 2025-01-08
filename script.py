64c250e700947a0b46c37ed444fac63c4a33da93
Victor Aguiar <v.aguiar.dev@gmail.com>
2025-01-08 20:43:57 -0300

    feat: converts colod image to grayscale (with np)

--- script.py
+++ script.py
@@ -22,7 +22,29 @@ def upload_images():
 
     return img_pth
 
+# Function that reduces the color space dimensionality of an image (color -> grayscale)
+def convert_to_grayscale(img_pth):
+  img = Image.open(img_pth) # Open Image
+  img_array = np.array(img) # Convert Image to Array
+
+  # Checks if the image is colored
+  if len(img_array.shape) == 3:
+    # Convert the image to grayscale
+    gray_img = np.dot(img_array[...,:3], [0.2989, 0.5870, 0.1140])
+  else:
+    gray_img = img_array
+  
+  # Display the grayscale image
+  plt.imshow(gray_img, cmap='gray')
+  plt.axis('off')
+  plt.show()
+
+  return gray_img
+
 # --------------------------- Main ---------------------------
 
 # Upload Image
 img_pth = upload_images()
+
+# Convert Image to Grayscale
+gray_img = convert_to_grayscale(img_pth)
