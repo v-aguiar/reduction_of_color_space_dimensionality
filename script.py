f4790b31037894d43f9a1f2acb2955d075c1b049
Victor Aguiar <v.aguiar.dev@gmail.com>
2025-01-08 21:03:14 -0300

    feat: add button to download both images + added example and generated images

--- script.py
+++ script.py
@@ -57,23 +57,35 @@ def convert_to_binary(gray_img, threshold=128):
 def save_image(img, filename):
     Image.fromarray(img.astype(np.uint8)).save(filename)
 
-# Function that allows the user to download the image
-def create_download_button(binary_img, filename):
+# Function to create download buttons
+def create_download_button(gray_img, binary_img):
     root = tk.Tk()
-    root.title("Download da Imagem")
+    root.title("Download das Imagens")
     
-    # Download function
-    def on_download():
-        save_image(binary_img, filename)
-        print(f"Imagem salva como {filename}. Você pode baixá-la agora.")
-        root.quit()  # Close window after download
+    # Download function for grayscale image
+    def on_download_gray():
+        save_image(gray_img, './images/grayscale_image.png')
+        print("Imagem em Tons de Cinza salva como grayscale_image.png. Você pode baixá-la agora.")
     
-    # Create download button
-    download_button = tk.Button(root, text="Salvar Imagem em Preto e Branco", command=on_download)
-    download_button.pack(pady=20)
+    # Download function for binary image
+    def on_download_binary():
+        save_image(binary_img, './images/binary_image.png')
+        print("Imagem Binária salva como binary_image.png. Você pode baixá-la agora.")
     
+    # Button to save grayscale image
+    download_button_gray = tk.Button(root, text="Salvar Imagem em Tons de Cinza", command=on_download_gray)
+    download_button_gray.pack(pady=10)
+    
+    # Button to save binary image 
+    download_button_binary = tk.Button(root, text="Salvar Imagem Binária (Preto e Branco)", command=on_download_binary)
+    download_button_binary.pack(pady=10)
+    
+    # Finish execution when closing the window
+    root.protocol("WM_DELETE_WINDOW", root.quit)
+
     # Starts UI
     root.mainloop()
+
 
 # --------------------------- Main ---------------------------
 
@@ -87,5 +99,6 @@ gray_img = convert_to_grayscale(img_pth)
 binary_img = convert_to_binary(gray_img)
 
 # Download Image
-create_download_button(binary_img, 'binary_image.jpg')
+create_download_button(gray_img, binary_img)
+
 
